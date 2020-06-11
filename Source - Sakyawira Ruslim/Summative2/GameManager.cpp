816ce937e86654592203ace953e69efe946753ae
Sakyawira <sakyawira@gmail.com>
2020-06-12 10:14:50 +1200

    animated model shadow is rendered

--- Source - Sakyawira Ruslim/Summative2/GameManager.cpp
+++ Source - Sakyawira Ruslim/Summative2/GameManager.cpp
@@ -222,10 +222,11 @@ void GameManager::render()
 		frame_counts_ += 1.0f * m_clock_->GetDeltaTick() * 120.0f;
 
 		m_shadowMap->start();
+		m_skModel->ShadowPass(m_shadowMap, m_clock_->GetDeltaTick(), m_mesh_terrain);
 		stencilCube->DrawShadow(m_shadowMap, camera, "currentTime", current_time_, "frameCounts", static_cast<int>(frame_counts_), m_clock_->GetDeltaTick());
 		m_mesh_terrain->ShadowPass(glm::vec3(0.0f, -50.0f, 0.0f), m_shadowMap);
 		//m_mesh_terrain->render(glm::vec3(0.0f, -50.0f, 0.0f), m_shadowMap);
-		m_skModel->ShadowPass(m_shadowMap,m_clock_->GetDeltaTick(), m_mesh_terrain);
+	
 		
 		m_shadowMap->end();
 	//	m_frameBuffer->PrepareRender();
